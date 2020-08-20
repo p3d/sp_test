c7704cd4e0bbd8cc46d2059e61a3d57de9b86b64
paul dalton <paul@p3d.co.uk>
2020-08-20 16:01:22 +0100

    Remove exception handling

--- lib/web_server_log_parser.rb
+++ lib/web_server_log_parser.rb
@@ -9,21 +9,17 @@ class WebServerLogParser
   end
 
   def parse(filename)
-    begin
-      log_file = File.open(filename, 'r')
-      log_data = log_file.readlines
-      
-      log_data.each do |log_line|
-        page_name, visitor_address = log_line.split(' ')
-        visit = Visit.new(page_name, visitor_address)
+    log_file = File.open(filename, 'r')
+    log_data = log_file.readlines
+    
+    log_data.each do |log_line|
+      page_name, visitor_address = log_line.split(' ')
+      visit = Visit.new(page_name, visitor_address)
 
-        if @pages[page_name] == nil
-          @pages[page_name] = Page.new(page_name)
-        end
-        @pages[page_name].visits << visit
+      if @pages[page_name] == nil
+        @pages[page_name] = Page.new(page_name)
       end
-    rescue Errno::ENOENT => e
-      return false
+      @pages[page_name].visits << visit
     end
     true
   end
